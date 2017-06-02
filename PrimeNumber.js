8ca38bfc93bbd711ebd731fa20788be06e56ade5
Debmalya Jash <debmalya.jash@gmail.com>
2017-06-02 22:45:18 +0800

    Update PrimeNumber.js

--- PrimeNumber.js
+++ PrimeNumber.js
@@ -5,31 +5,30 @@ exports.handler = (event, context, callback) => {
     var num = Number(event.currentIntent.slots.Integer);
     status = status.concat(num);
     console.log("number :", num);
-    if (num < 0){
-        callback(null, "Please enter a positive number.");
-    }
-    var square_root = Math.sqrt(num);
-    var prime = true;
-    if (num === 1){
-        prime = false;
-    } else {
-        for (var i = 2; i <= square_root; i++){
-            if (num % i === 0){
-                prime = false;
-                break;
+    if (num < 1){
+        status = "Please enter a positive number.";
+    }else {
+        var square_root = Math.sqrt(num);
+        var prime = true;
+        if (num === 1){
+            prime = false;
+        } else {
+            for (var i = 2; i <= square_root; i++){
+                if (num % i === 0){
+                    prime = false;
+                    break;
+                }
             }
         }
     }
     
     if (!prime){
-        console.log(num, " is not prime");
         status = status.concat(" is not a prime number");
     } else {
-        console.log(num, " is prime");
         status = status.concat(" is a prime number");
     }
     
-    console.log("status",status);
+    
     callback(null, {
         "dialogAction": {
           "type": "Close",
